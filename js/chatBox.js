1121c17fa333ee37644c182586f978378f378e72
udaysai194 <udaysai194@gmail.com>
2018-04-02 18:33:39 +0530

    commited

---
+++ js/chatBox.js
@@ -0,0 +1,22 @@
+function msg() {
+	if (document.getElementById('msgInput').value != '') {
+		var div = document.createElement("div");
+		var span = document.createElement("span");
+		var message = document.getElementById("msgInput").value;
+		var node = document.createTextNode(message);
+		var element = document.getElementById("chat-box");
+		element.appendChild(div).appendChild(span).appendChild(node);
+		document.getElementById("msgInput").value = '';
+		var element = document.getElementById("chat-box");
+		element.scrollTop = element.scrollHeight;
+	}else{}
+	}
+
+var input = document.getElementById("msgInput");
+	input.addEventListener("keyup", function(event) {
+    	event.preventDefault();
+    	if (event.keyCode === 13) {
+        	document.getElementById("send").click();
+    	}
+	});
+
